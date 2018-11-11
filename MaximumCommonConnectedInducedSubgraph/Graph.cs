785f08867645f0a24f315c3af99c3e6f5801e897
Gustaw Żyngiel <gustaw.zyngiel@gmail.com>
2018-11-11 19:05:33 +0100

    add Graph class: reading csv and printing graph implementations

--- MaximumCommonConnectedInducedSubgraph/Graph.cs
+++ MaximumCommonConnectedInducedSubgraph/Graph.cs
@@ -1,10 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace MaximumCommonConnectedInducedSubgraph
 {
-    class Graph
+    public class Graph
     {
+        private int[,] _graph;        
+        private int _size;
+
+        public void FillEdgesFromCsv(string csvPath)
+        {
+            var file = File.ReadAllLines(csvPath);
+            _size = file.Length;
+            var g = new int[_size, _size];
+
+            for (int i=0; i < _size; i++)
+            {
+                var currentLine = file[i].Split(',');
+                for (int j = 0; j < currentLine.Length; j++)
+                {
+                    g[i, j] = currentLine[j].Equals("1") ? 1 : 0;
+                }
+            }
+            _graph = g;
+        }
+
+        public void PrintGraph()
+        {
+            for(int i=0; i < _graph.GetLength(0); i++)
+            {
+                for(int j=0; j < _graph.GetLength(0); j++)
+                {
+                    if(j>0)
+                    {
+                        Console.Write(",");
+                    }
+                    Console.Write(_graph[i, j]);                                        
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
