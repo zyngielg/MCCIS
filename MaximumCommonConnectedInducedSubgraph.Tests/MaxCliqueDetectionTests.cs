b27fad395043224b7dc16808881080a89f2fb1fd
Gustaw Żyngiel <gustaw.zyngiel@gmail.com>
2018-11-18 15:52:55 +0100

    add test for clique detection

---
+++ MaximumCommonConnectedInducedSubgraph.Tests/MaxCliqueDetectionTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MaximumCommonConnectedInducedSubgraph.Tests
+{
+
+    public class MaxCliqueDetectionTests
+    {
+        [Theory]
+        [InlineData("\\..\\..\\..\\Graphs\\a2.csv")]
+        public void MaxCliqueDetectionSingleGraph(string gName)
+        {
+            var expectedResult = new List<int> { 1, 2, 4 };
+
+            var gPath = Environment.CurrentDirectory + gName;
+
+
+            var g = new Graph();
+
+
+            g.FillEdgesFromCsv(gPath);
+
+            var alg = new ModularGraphMaxCliqueAlgorithm();
+            alg.MaxCliquePolynomial(g);
+
+            #region Then
+            Assert.Equal(expectedResult.Count, alg._maxCP.Count);
+            for (int i = 0; i < expectedResult.Count; i++)
+            {
+                Assert.Equal(expectedResult[i], alg._maxCP[i]);
+            }
+            #endregion
+
+
+        }
+    }
+}
