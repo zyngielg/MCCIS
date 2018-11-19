0f0ef0b77d2525950ca9e70fa886623f3f7f1a02
Gustaw Żyngiel <gustaw.zyngiel@gmail.com>
2018-11-19 15:08:07 +0100

    add requirements for McGregorAlgorithm to fit the structure of main

--- MaximumCommonConnectedInducedSubgraph/McGregorAlgorithm.cs
+++ MaximumCommonConnectedInducedSubgraph/McGregorAlgorithm.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace MaximumCommonConnectedInducedSubgraph
 {
-    class McGregorAlgorithm
+    public class McGregorAlgorithm : IAlgorithm
     {
+        private bool _onlyVertices;
+
+        public McGregorAlgorithm(bool onlyVertices)
+        {
+            _onlyVertices = onlyVertices;
+        }
+        public (int[], int[]) GetMaximalCommonSubgraphMapping(string g1Path, string g2Path)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
