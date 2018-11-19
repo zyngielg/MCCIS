efcc8bbca8b3f87350ba592541c01a9f47ab07b1
Gustaw Żyngiel <gustaw.zyngiel@gmail.com>
2018-11-19 15:06:33 +0100

    replace several ifs with switch, code cleanup

--- MaximumCommonConnectedInducedSubgraph/Program.cs
+++ MaximumCommonConnectedInducedSubgraph/Program.cs
@@ -6,14 +6,13 @@ namespace MaximumCommonConnectedInducedSubgraph
     {
         static void Main(string[] args)
         {
-            if(args.Length == 0 || args.Length != 3)
+            string parametersUsage = "  <alg_opt>:\n\t 0: accurate McGregor algorithm\n\t 1: approximate algorithm based on finding maximal clique in a modular product of two input graphs\n\t 2: approximate algorithm based on merging cliques created from input graphs";
+
+            if (args.Length == 0 || args.Length != 3)
             {
                 Console.WriteLine("Usage:");
                 Console.WriteLine("dotnet run <alg_opt> <g1> <g2>");
-                Console.WriteLine("  <alg_opt>:");
-                Console.WriteLine("\t 0: accurate McGregor algorithm");
-                Console.WriteLine("\t 1: approximate algorithm based on finding maximal clique in a modular product of two input graphs");
-                Console.WriteLine("\t 2: approximate algorithm based on merging cliques created from input graphs");
+                Console.WriteLine(parametersUsage);
                 Console.WriteLine("  <g1>:");
                 Console.WriteLine("\t name of G1");
                 Console.WriteLine("  <g2>:");
@@ -21,38 +20,61 @@ namespace MaximumCommonConnectedInducedSubgraph
 
                 return;
             }
+            int algorithmFlag;
+
+            try
+            {
+                algorithmFlag = Convert.ToInt32(args[0]);
+            }
+            catch
+            {
+                Console.WriteLine(parametersUsage);
+                return;
+            }
             
-            var algorithmFlag = Convert.ToInt32(args[0]);
             IAlgorithm alg;
             var g1Path = Environment.CurrentDirectory + "\\Graphs\\" + args[1];
             var g2Path = Environment.CurrentDirectory + "\\Graphs\\" + args[2];
 
-            if (algorithmFlag == 0)
-            {
-                Console.WriteLine("******ACCURATE ALGORITHM********");
-                alg = new McGregorAlgorithm();
-            }
-            else if(algorithmFlag == 1)
-            {
-                Console.WriteLine("******APPROXIMATE ALGORITHM******");
-                Console.WriteLine("Description: algorithm finding the maximal clique in a modular product of input graphs");
-                alg = new ModularGraphMaxCliqueAlgorithm();
+            string title;
+            string criterion;
+            string description;
 
-            }
-            else if(algorithmFlag == 2)
+            switch (algorithmFlag)
             {
-                Console.WriteLine("******APPROXIMATE ALGORITHM******");
-                Console.WriteLine("Description: algorithm finding the maximal clique in a modular product of input graphs");
-                alg = new ModularGraphMaxCliqueAlgorithm();
+                case 0:
+                    alg = new McGregorAlgorithm(true);
+                    title = "******ACCURATE ALGORITHM********";
+                    criterion = "Criterion: number of vertices.";
+                    description = "McGregor's algorithm.";
+                    break;
+                case 1:
+                    alg = new McGregorAlgorithm(false); 
+                    title = "******ACCURATE ALGORITHM********";
+                    criterion = "Criterion: sum of vertices and edges.";
+                    description = "McGregor's algorithm.";
+                    break;
+                case 2:
+                    alg = new ModularGraphMaxCliqueAlgorithm();
+                    title = "******APPROXIMATE ALGORITHM******";
+                    criterion = "Criterion: number of vertices.";
+                    description = "Description: algorithm finding the maximal clique in a modular product of input graphs";
+                    break;
+                case 3:
+                    alg = new LinkingCliquesAlgorithm();
+                    title = "******APPROXIMATE ALGORITHM******";
+                    criterion = "Criterion: number of vertices.";
+                    description = "Description: algorithm finding the maximal clique in a modular product of input graphs";
+                    break;
+                default:
+                    Console.WriteLine(parametersUsage);
+                    return;
             }
-            else
-            {
-                Console.WriteLine("  <alg_opt>:");
-                Console.WriteLine("\t 0: accurate McGregor algorithm");
-                Console.WriteLine("\t 1: approximate algorithm based on finding maximal clique in a modular product of two input graphs");
-                Console.WriteLine("\t 2: approximate algorithm based on merging cliques created from input graphs");
-                return;
-            }
+    
+            Console.WriteLine();
+            Console.WriteLine(title);
+            Console.WriteLine(criterion);
+            Console.WriteLine(description);
             Console.WriteLine();
 
             int[] g1Mapping;
@@ -60,6 +82,7 @@ namespace MaximumCommonConnectedInducedSubgraph
 
             (g1Mapping, g2Mapping) = alg.GetMaximalCommonSubgraphMapping(g1Path, g2Path);
 
+            Console.WriteLine("******RESULTS******");
             Console.Write("G1:");
             for(int i=0; i<g1Mapping.Length; i++)
             {
